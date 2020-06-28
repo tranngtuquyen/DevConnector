ef1da45d3abc5cf869c370862e281b2a77a8995a
Quyen Tran <tranngtuquyen@gmail.com>
2020-06-28 14:48:34 -0700

    Create layout and auth components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login}/>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
